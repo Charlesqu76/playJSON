bb2f475fc55d6219567f305d8343d4f787e772a2
charles qu <Charlesqu76@outlook.com>
2025-04-01 23:28:35 +1100

    fix: init

--- web/src/components/Graph.tsx
+++ web/src/components/Graph.tsx
@@ -1,13 +1,18 @@
 import { useStore } from "@/store";
 import React, { useEffect } from "react";
+import data from "@/data.json";
 export default function Graph() {
   const ref = React.useRef<HTMLDivElement>(null);
   const { graph } = useStore((state) => state);
   useEffect(() => {
     if (ref.current) {
       graph.initCanvas(ref.current);
-      const initialData = JSON.parse(localStorage.getItem("json") || "{}");
-      graph.recover(initialData);
+      const json = localStorage.getItem("json");
+      if (json) {
+        graph.recover(JSON.parse(json));
+      } else {
+        graph.initData(data);
+      }
       document.addEventListener("keydown", (e) => {
         if (e.metaKey && e.key === "s") {
           e.preventDefault();
